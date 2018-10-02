3b235bbe4e598c8a24d0f6bca8a35c7a68cd988d
Steven Kirk <grokys@users.noreply.github.com>
2018-10-02 13:15:01 +0200

    Modifications for use in GHfVS.

--- src/Akavache.Core/Properties/AssemblyInfo.cs
+++ src/Akavache.Core/Properties/AssemblyInfo.cs
@@ -2,6 +2,11 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: InternalsVisibleTo("Akavache.Tests")]
-[assembly: InternalsVisibleTo("Akavache.Sqlite3")]
-[assembly: InternalsVisibleTo("Akavache.Mobile")]
+//[assembly: InternalsVisibleTo("Akavache.Tests")]
+[assembly: InternalsVisibleTo(@"Akavache.Sqlite3,PublicKey=" +
+"0024000004800000940000000602000000240000525341310004000001000100619ac77cde677e" +
+"6136464deac723fa893a3a469672196a1539c1b3af5e5bf572ad0d287dae773152cd688d6a8bb3" +
+"a6e7c440fc4f30f84d6dc53c02e1747c3b41668c5d5d6fb1efbb70662ec301b63b6639f70236b0" +
+"c9602e3351b83362e857b0e547712ebbda6bf9d484109cb74a76cf5e72cbd08bd56269c0f57fcf" +
+"45b7c392")]
+//[assembly: InternalsVisibleTo("Akavache.Mobile")]
